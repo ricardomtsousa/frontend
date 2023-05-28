3ac891e0942547e65c5f057159669f43c7d89b87
ricardo <estgv18471@alunos.estgv.ipv.pt>
2023-05-28 20:27:51 +0100

    k

--- src/components/PasswordReset/passwordResetCardComponent.jsx
+++ src/components/PasswordReset/passwordResetCardComponent.jsx
@@ -1,21 +1,49 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {MDBCardBody, MDBInput, MDBCheckbox, MDBCard } from 'mdb-react-ui-kit';
+import { MDBCardBody, MDBInput, MDBCheckbox, MDBCard } from 'mdb-react-ui-kit';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import './styles.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Articles from '../Articles';
 import api from '../../services/api';
 import { useHistory } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import { Button } from 'react-bootstrap';
 import * as React from 'react';
 import ToastSnackBar from '../SnackBar/toastSnackBar';
+import { useLocation } from 'react-router-dom';
 
 
 const ResetPasswordCardComponent = () => {
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setconfirmPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [userId, setUserId] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userIdd = urlParams.get('userId');
+    const tokenn = decodeURIComponent(urlParams.get('token'));
+
+    setUserId(userIdd);
+    setToken(tokenn);
+  }, []);
+
+  const resetPassword = () => {
+    if (password !== confirmPassword) {
+      // Passwords don't match, handle the error (e.g., show error message)
+      return;
+    }
+
+    api.post('/password-reset', { userId, token, password })
+      .then(response => {
+        // Handle the response (e.g., show success message, redirect to login page)
+      })
+      .catch(error => {
+        // Handle the error (e.g., show error message)
+      });
+  };
 
   return (
 
@@ -23,16 +51,16 @@ const ResetPasswordCardComponent = () => {
       <MDBCard style={{ paddingLeft: '5%', paddingRight: '5%', paddingTop: '10%', paddingBottom: '10%' }}>
         <MDBCardBody>
 
-          <form  className="">
+          <form onSubmit={resetPassword} className="">
             <h2>Recuperar Password</h2>
             <div>
-            <MDBInput wrapperClass='mb-4' required placeholder='Nova Password' id='formControlLg' type='password' size="lg" value={password} onChange={e => setPassword(e.target.value)} />
-              
-              <MDBInput wrapperClass='mb-4' required placeholder='Confirmar Password' id='formControlLg' type='password' size="lg" value={confirmPassword} onChange={e => setconfirmPassword(e.target.value)} />
-              
+              <MDBInput wrapperClass='mb-4' required placeholder='Nova Password' id='formControlLg' type='password' size="lg" value={password} onChange={e => setPassword(e.target.value)} />
+
+              <MDBInput wrapperClass='mb-4' required placeholder='Confirmar Password' id='formControlLg' type='password' size="lg" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+
             </div>
 
-           
+
 
             <div className='text-md-start mt-2  text-start'>
               <button type="submit" class="btn warning-button">Entrar</button>
